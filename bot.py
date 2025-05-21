b884748a6089ee6a10ea777f393501cad3658a77
BelabbasMohammadQassem <mohammadqassem.93390.b@gmail.com>
2025-05-21 12:58:36 +0200

    modification des arguments

--- bot.py
+++ bot.py
@@ -16,10 +16,6 @@ SALON_SURVEILLE_ID = 1102350490416709672
 LOGICIEL_CHANNEL_ID = 1310765800835256350
 MATERIEL_CHANNEL_ID = 697726206891655168
 
-# Mots et expressions pour détecter les questions
-MOTS_QUESTION = ["comment", "pourquoi", "qui", "quand", "où", "quoi", "quel", "quelle", 
-                "quels", "quelles", "est-ce que", "est ce que"]
-
 # Mots et expressions pour détecter les demandes d'aide
 MOTS_AIDE = [
     "aide", "aider", "aidez", "help", "besoin", "problème", "probleme", "bug",
@@ -32,28 +28,6 @@ MOTS_AIDE = [
     "issue", "troubleshoot", "fix", "broken", "stuck", "struggle", "struggling",
     "can't", "cant", "unable to", "help me", "anyone know", "quelqu'un sait"
 ]
-
-SIMILARITE_THRESHOLD = 0.75
-
-def est_similaire_a_un_mot_question(mot, longueur_minimum=3):
-    """Vérifie si un mot est similaire à un mot de question connu."""
-    if len(mot) < longueur_minimum:
-        return False
-        
-    for mot_question in MOTS_QUESTION:
-        similarite = Levenshtein.ratio(mot, mot_question)
-        if similarite >= SIMILARITE_THRESHOLD:
-            return True
-    return False
-
-def contient_mot_question_avec_fautes(texte):
-    """Analyse le texte pour trouver des mots similaires aux mots de questions."""
-    mots = texte.lower().split()
-    
-    for i in range(min(3, len(mots))):
-        if est_similaire_a_un_mot_question(mots[i]):
-            return True
-    return False
 
 def contient_demande_aide(texte):
     """Vérifie si le texte contient des mots ou expressions liés à une demande d'aide."""
@@ -86,32 +60,9 @@ async def on_message(message):
     if message.channel.id == SALON_SURVEILLE_ID:
         contenu = message.content.lower()
         
-        est_question = False
-        est_demande_aide = False
-        
-        # Vérifier si c'est une question basée sur la ponctuation ou les mots interrogatifs
-        if "?" in contenu:
-            est_question = True
-        
-        for mot in MOTS_QUESTION:
-            if contenu.startswith(mot + " "):
-                est_question = True
-                break
-        
-        if not est_question and len(contenu) > 5:  
-            est_question = contient_mot_question_avec_fautes(contenu)
-        
         # Vérifier si c'est une demande d'aide
-        if not est_question:
-            est_demande_aide = contient_demande_aide(contenu)
-        
-        # Répondre si c'est une question ou une demande d'aide
-        if est_question or est_demande_aide:
-            if est_question:
-                response = f"{message.author.mention}, merci d'utiliser les salons <#{LOGICIEL_CHANNEL_ID}>  ou <#{MATERIEL_CHANNEL_ID}> pour vos questions."
-            else:
-                response = f"{message.author.mention}, j'ai détecté que vous avez besoin d'aide. Merci de poster votre demande dans <#{LOGICIEL_CHANNEL_ID}> ou <#{MATERIEL_CHANNEL_ID}>."
-            
+        if contient_demande_aide(contenu):
+            response = f"{message.author.mention}, j'ai détecté que vous avez besoin d'aide. Merci de poster votre demande dans <#{LOGICIEL_CHANNEL_ID}> ou <#{MATERIEL_CHANNEL_ID}>."
             await message.reply(response)
     
     await bot.process_commands(message)
